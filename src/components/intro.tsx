a80cbbbd534d78853a1022beaf5be8715f9097cd
Aaron Hong <46896330+AaronHongry@users.noreply.github.com>
2025-01-27 16:22:44 -0500

    created intro

---
+++ src/components/intro.tsx
@@ -0,0 +1,9 @@
+const Intro = () => {
+    return (
+        <div className="py-32 pt-32">
+            <h1 className="text-7xl font-semibold">Hi there!</h1>
+        </div>
+    );
+}
+
+export default Intro;
---
+++ src/components/navbar.tsx
@@ -0,0 +1,10 @@
+const NavBar = () => {
+    return (
+        <div className="flex flex-row gap-4">
+            <p>LinkedIn</p>
+            <p>Github</p>
+        </div>
+    );
+}
+
+export default NavBar;
