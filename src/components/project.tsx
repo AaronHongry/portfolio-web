e84b2570d82178ad5946ba8100492aa630d06e2a
Aaron Hong <46896330+AaronHongry@users.noreply.github.com>
2025-02-25 16:30:46 -0500

    Update project.tsx

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -15,7 +15,7 @@ interface ProjectProps {
     onClick: () => void;
 }
 
-const Project: React.FC<ProjectProps> = ({layoutCardId, name, description, onClick}) => {
+const Project: React.FC<ProjectProps> = ({layoutCardId, name, description, picUrl, onClick}) => {
 
     return (
         <div className="relative">
@@ -25,16 +25,15 @@ const Project: React.FC<ProjectProps> = ({layoutCardId, name, description, onCli
             transition={{ease: "anticipate", duration: 0.5}}
             className={`relative flex flex-col w-72 h-72 rounded-xl overflow-hidden transition-shadow shadow-md hover:shadow-2xl`}
             onClick={onClick}>
-                <motion.div initial={{opacity: 0}} whileHover={{opacity: 0.1}} className="absolute w-full h-full bg-white z-20"/>
+                {/*<motion.div initial={{opacity: 0}} whileHover={{opacity: 0.1}} className="absolute w-full h-full bg-white z-20"/>*/}
                 <motion.div className="h-2/3">
-                    {/*picUrl.split(".").pop() == "mp4"
+                    {picUrl.split(".").pop() == "mp4"
                     ? (
                         <video src={`images/${picUrl}`} muted loop autoPlay playsInline className="w-full h-full object-cover"/>
                     ) 
                     : (
                         <Image alt={`${name} Picture`} width={288} height={216} src={`/images/${picUrl}`} />
-                    )*/}
-                    < Image alt={"name"} width={288} height={216} src={`/images/placeholder.gif`}/>
+                    )}
                 </motion.div>
                 <motion.div className="flex flex-col h-1/3 background justify-center px-3 gap-2 py-3">
                     <h1 className="text-lg font-semibold">{name}</h1>
